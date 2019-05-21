41bba55e81cebcb130fe5bdb6806641ad9aa36fc
makata <mail2teka@gmail.com>
2019-05-21 09:37:19 +0300

    set  default app version to 2.0

--- public/js/config.js
+++ public/js/config.js
@@ -12,9 +12,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
  
-// firebase.firestore().enablePersistence({ experimentalTabSynchronization: true })
-//   .then(() => { })
-//   .catch(function (err) { }); 
+ 
 // Google OAuth Client ID, needed to support One-tap sign-up. Set to null if One-tap sign-up is not supported.
 var CLIENT_ID = null;// 935579989609-s5e3urqeebs0eihp86kpavnl6f256rvv.apps.googleusercontent.com;
 //Web API Key AIzaSyD7mF7qm-KAPnWcJfVii48077aZzLHNH7Q
